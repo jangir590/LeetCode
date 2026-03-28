27166f23d55f3e08891463499965d880edfc576d
jangir590 <jangirsandy862@gmail.com>
2026-03-28 19:28:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.6 MB (100.00%)

---
+++ 0231-power-of-two/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        while(n>1)
+        {
+            int rem = n%2;
+            n=n/2;
+            if(rem!=0){
+                return false;
+            }
+
+        }
+        return true;
+        
+    }
+};
