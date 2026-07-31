310dda03e295994b8462ad1e871e07c1bb24cde6
jangir590 <jangirsandy862@gmail.com>
2026-07-31 05:36:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.7 MB (93.77%)

---
+++ 0014-longest-common-prefix/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        if(n==0) return "";
+        for(int i = 0 ; i<strs[0].length() ;i++)
+
+        {   
+            char c = strs[0][i];
+
+            for(int j =1 ; j<strs.size() ; j++ )
+
+            {
+                if(i>+strs[j].length() || strs[j][i]!=c)
+                {
+                    return strs[0].substr(0,i);
+                }
+
+            }
+
+        } return strs[0];
+        
+        
+    }
+};
