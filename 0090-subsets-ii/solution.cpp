9d385e6f69d3bfac6d352268af44db5a8c0b6223
jangir590 <jangirsandy862@gmail.com>
2026-08-09 14:07:56 +0000

    Sync LeetCode submission Runtime - 3 ms (31.38%), Memory - 10.5 MB (68.31%)

---
+++ 0090-subsets-ii/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+     void findCombination(int idx ,vector<int>& nums ,vector<vector<int>>& ans , vector<int>& ds){
+
+        ans.push_back(ds);
+        for(int i =idx ; i<nums.size();i++){
+            if(i>idx && nums[i]==nums[i-1])continue;
+            ds.push_back(nums[i]);
+            findCombination(i+1,nums,ans,ds);
+            ds.pop_back();
+
+        }
+        
+
+
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        vector<int>ds;
+        findCombination(0,nums,ans,ds);
+        return ans;
+        
+    }
+};
