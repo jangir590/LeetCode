36573a17dd33723ad0096097d01795ba2aa139fa
jangir590 <jangirsandy862@gmail.com>
2026-08-15 16:39:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (74.89%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int MaxiAmount(vector<int>& nums) {
+        int n =nums.size();
+        int prev =nums[0];
+        int prev2 =0;
+
+        for(int i=1 ;i<n ;i++){
+            int take =nums[i];
+            if(i>1){
+                take+=prev2;
+            }
+            int nottake = prev ;
+            int curi =max(take,nottake);
+            prev2=prev;
+            prev=curi;
+
+        }
+        return prev;
+
+        
+    }
+
+    long long int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        vector<int>temp1,temp2;
+
+        for(int i=0 ; i<n ; i++){
+            if(i!=0){
+                temp1.push_back(nums[i]);
+            }
+            if(i!=n-1){
+                temp2.push_back(nums[i]);
+            }
+        }
+
+        int ans1 = MaxiAmount(temp1);
+        int ans2 = MaxiAmount(temp2);
+      
+        return max(ans1,ans2);
+        
+        
+    }
+};
